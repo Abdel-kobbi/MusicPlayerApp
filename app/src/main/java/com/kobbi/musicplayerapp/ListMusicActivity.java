a377e6eebf11e32574115cc9abfa1d7020acd847
abdel-kobbi <mr.abdelkhaliqkobbi@gmail.com>
2025-03-15 22:28:14 +0000

    feat: add necessary permission to read media audio

--- app/src/main/java/com/kobbi/musicplayerapp/ListMusicActivity.java
+++ app/src/main/java/com/kobbi/musicplayerapp/ListMusicActivity.java
@@ -1,10 +1,13 @@
 package com.kobbi.musicplayerapp;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -14,6 +17,7 @@ import java.util.List;
 
 public class ListMusicActivity extends AppCompatActivity {
 
+    private final int REQUEST_PERMISSION = 100;
     private List<Song> songList;
     private ListView lvSongs;
 
@@ -31,10 +35,33 @@ public class ListMusicActivity extends AppCompatActivity {
         lvSongs = findViewById(R.id.lvSongs);
         songList = new ArrayList<>();
 
+        if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.DYNAMIC_RECEIVER_NOT_EXPORTED_PERMISSION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.DYNAMIC_RECEIVER_NOT_EXPORTED_PERMISSION}, REQUEST_PERMISSION);
+            return;
+        } else {
+            // we have permission to read from external storage
+            getSongs();
+        }
+
         SongsAdapter songsAdapter = new SongsAdapter(getApplicationContext(), songList);
 
         lvSongs.setEmptyView(findViewById(R.id.lvEmpty));
 
         lvSongs.setAdapter(songsAdapter);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_PERMISSION) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getSongs();
+            }
+        }
+    }
+
+    private void getSongs() {
+        // read songs from external storage
+
     }
 }
