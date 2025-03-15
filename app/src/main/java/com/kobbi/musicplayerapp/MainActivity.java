98105fc24086a33839abe464420de8bfcd6ecb0d
abdel-kobbi <mr.abdelkhaliqkobbi@gmail.com>
2025-03-15 01:24:04 +0000

    feat: display music duration and add seekBar control

--- app/src/main/java/com/kobbi/musicplayerapp/MainActivity.java
+++ app/src/main/java/com/kobbi/musicplayerapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -35,13 +36,16 @@ public class MainActivity extends AppCompatActivity {
         seekBarTime = findViewById(R.id.seekBarTime);
         seekBarVolume = findViewById(R.id.seekBarVolume);
         tvTime = findViewById(R.id.tvTime);
-        tvTime = findViewById(R.id.tvDuration);
+        tvDuration = findViewById(R.id.tvDuration);
         btnPlay = findViewById(R.id.btnPlay);
 
         mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.music);
         mediaPlayer.setLooping(true);
         mediaPlayer.seekTo(0);
         mediaPlayer.setVolume(0.5f, 0.5f);
+
+        String duration = millisecondToString(mediaPlayer.getDuration());
+        tvDuration.setText(duration);
 
         seekBarVolume.setProgress(50);
 
@@ -63,6 +67,28 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        seekBarTime.setMax(mediaPlayer.getDuration());
+        seekBarTime.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if(fromUser){
+                    mediaPlayer.seekTo(progress);
+                    seekBarTime.setProgress(progress);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
         btnPlay.setOnClickListener(v -> {
             if (mediaPlayer.isPlaying()) {
                 mediaPlayer.pause();
@@ -72,5 +98,18 @@ public class MainActivity extends AppCompatActivity {
                 btnPlay.setBackgroundResource(R.mipmap.ic_pause);
             }
         });
+
+    }
+
+    private String millisecondToString(int time) {
+        String elapsedTime = "";
+        int minutes = time / 1000 / 60;
+        int seconds = time / 1000 % 60;
+        elapsedTime = minutes + ":";
+        if (seconds < 10) {
+            elapsedTime += "0";
+        }
+        elapsedTime += seconds;
+        return elapsedTime;
     }
 }
